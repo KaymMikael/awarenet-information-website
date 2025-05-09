099ddf88b7920ce1e4ce1095875d3a0374d97f11
Sero <kmcaltiz@gmail.com>
2025-05-09 17:12:03 +0800

    setup toaster

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,17 @@
 import { Route, Routes } from "react-router";
 import HomePage from "./pages/HomePage";
 import SurveyPage from "./pages/SurveyPage";
+import { Toaster } from "@/components/ui/sonner";
 
 const App = () => {
   return (
-    <Routes>
-      <Route path="/" element={<HomePage />} />
-      <Route path="/survey" element={<SurveyPage/>}/>
-    </Routes>
+    <>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/survey" element={<SurveyPage />} />
+      </Routes>
+      <Toaster />
+    </>
   );
 };
 
