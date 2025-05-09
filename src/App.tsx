f247e0d872101f71abc940e33ea1937bcc8bc216
Sero <kmcaltiz@gmail.com>
2025-05-10 06:48:53 +0800

    update: App.tsx create a new route /feedback

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { Route, Routes } from "react-router";
 import HomePage from "./pages/HomePage";
 import SurveyPage from "./pages/SurveyPage";
 import { Toaster } from "@/components/ui/sonner";
+import FeedbackPage from "./pages/FeedbackPage";
 
 const App = () => {
   return (
@@ -9,6 +10,7 @@ const App = () => {
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/survey" element={<SurveyPage />} />
+        <Route path="/feedback" element={<FeedbackPage />} />
       </Routes>
       <Toaster />
     </>
