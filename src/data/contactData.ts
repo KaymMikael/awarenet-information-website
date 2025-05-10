aea4acc0ba6cdcb8914845fc4f4e5ad230903a08
Sero <kmcaltiz@gmail.com>
2025-05-10 20:42:44 +0800

    create: contactData.ts

---
+++ src/data/contactData.ts
@@ -0,0 +1,34 @@
+export const projectDevelopers = [
+  {
+    name: "Altiz, Khaim Michael C.",
+    email: "24-37827@g.batstate-u.edu.ph",
+  },
+  {
+    name: "Barza, Krista Mourine N.",
+    email: "24-34182@g.batstate-u.ph",
+  },
+  {
+    name: "Caindoy, Daniel Rodge A.",
+    email: "24-31428@g.batstate-u.ph",
+  },
+  {
+    name: "Catibog, Tristan Jace G.",
+    email: "24-33498@g.batstate-u.ph",
+  },
+  {
+    name: "Cortez, Lanz Ronan T.",
+    email: "24-30368@g.batstate-u.ph",
+  },
+  {
+    name: "Cuya, Julianne Margarette G.",
+    email: "24-33716@g.batstate-u.ph",
+  },
+  {
+    name: "Mallari, Gian Carlo P.",
+    email: "24-37890@g.batstate-u.ph",
+  },
+  {
+    name: "Mendoza, Zhaider M.",
+    email: "24-32783@g.batstate-u.ph",
+  },
+];
