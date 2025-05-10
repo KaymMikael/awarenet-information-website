d55b660d774b476c8d245e0f3078d4ee85f66241
Sero <kmcaltiz@gmail.com>
2025-05-10 16:38:52 +0800

    create: ContactPage.tsx

---
+++ src/pages/ContactPage.tsx
@@ -0,0 +1,14 @@
+import PageHeader from "@/components/PageHeader";
+import PageLayout from "@/components/PageLayout";
+import SectionLayout from "@/components/SectionLayout";
+
+const ContactPage = () => {
+  return (
+    <PageLayout>
+      <PageHeader />
+      <SectionLayout>ContactPage</SectionLayout>
+    </PageLayout>
+  );
+};
+
+export default ContactPage;
