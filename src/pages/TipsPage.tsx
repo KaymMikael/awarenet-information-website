a41c2674104871b7bdba3408218c36d035658b74
Sero <kmcaltiz@gmail.com>
2025-05-10 08:06:03 +0800

    create TipsPage.tsx

---
+++ src/pages/TipsPage.tsx
@@ -0,0 +1,14 @@
+import PageHeader from "@/components/PageHeader";
+import PageLayout from "@/components/PageLayout";
+import SectionLayout from "@/components/SectionLayout";
+
+const TipsPage = () => {
+  return (
+    <PageLayout>
+      <PageHeader />
+      <SectionLayout>TipsPage</SectionLayout>
+    </PageLayout>
+  );
+};
+
+export default TipsPage;
